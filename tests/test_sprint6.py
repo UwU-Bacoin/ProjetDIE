f0035eaa669b7ed3e81052656bc776e196a0f219
sigmanificient <edhyjox@gmail.com>
2022-09-25 03:27:09 +0200

    :white_check_mark: Add test for sprint6

--- tests/test_sprint6.py
+++ tests/test_sprint6.py
@@ -1,3 +1,4 @@
+import re
 import importlib
 import builtins
 
@@ -9,3 +10,22 @@ def test_add(monkeypatch):
     importlib.import_module('src.sprint6.add')
 
     assert catcher[0] == 999999
+
+
+def test_api(monkeypatch):
+    monkeypatch.setenv('EMAIL', 'email_test')
+    monkeypatch.setenv('STUDENT_ID', '42')
+
+    api = importlib.import_module('src.sprint6.API')
+
+    assert api.sasdie.is_initialized
+
+    api.main()
+    s = api.sasdie.API
+
+    assert s.email == 'email_test'
+    assert s.student_id == '42'
+
+    assert s.connected
+    assert s.key != 0
+    assert re.search(r'<pre>(\d+)</pre>', s.published_content)[1] == '1'
