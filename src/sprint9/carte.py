70dcbf138f3b9dac1ad1d3d67c656e5b68c75783
sigmanificient <edhyjox@gmail.com>
2022-09-26 02:29:09 +0200

    :bulb: Add explanation comments

--- src/sprint9/carte.py
+++ src/sprint9/carte.py
@@ -2,15 +2,25 @@ import os
 
 try:
     import sasdie
-
+    # Lorsque que l'on importe la version originale de sasdie,
+    # on signale l'utilisation de celle-ci
     IS_REPLACEMENT = False
 
 except ImportError:
     import fake_sasdie as sasdie
-
+    # Si "sasdie" n'est pas trouvé, on importe la version locale
+    # https://github.com/UwU-Bacoin/ProjetDIE/tree/main/lib/fake_sasdie
     IS_REPLACEMENT = True
 
 if not IS_REPLACEMENT:
+    # Si le module importé n'est pas la version locale de sasdie,
+    # on applique des corrections sur le module afin de le rendre cohérent.
+
+    # ces corrections permettent au programme de marcher
+    # avec la version originale de Sasdie.
+
+    # On crée un wrapper autour de Sasdie pour rendre l'utilisation
+    #  plus simple et intuitif (moins de méthodes)
     class API(sasdie.Sasdie):
 
         def __init__(self, email, student_id):
@@ -18,23 +28,29 @@ if not IS_REPLACEMENT:
             self.setLogin(email)
             self.setPasswd(student_id)
 
+        # on renomme la méthode "publierpage_html" en anglais
         publish_webpage = sasdie.Sasdie.publierpage_html
 
 
     _s = sasdie.Sasdie()
+
+    # On créé un pseudo-object Map pour améliorer la lisibilité (Map <=> sasdie.Sasdie)
     sasdie.Map = lambda: (_s.creerCarte(), _s.macarte)[1]
+
+    # On y ajoute les méthodes d'ajout d'un cercle, d'un marker et de rendu
+    # pour constituer "groupe" de la même manière que Sasdie.macarte
     sasdie.Sasdie.add_circle = sasdie.Sasdie.ajoutCercleSurLaCarte
+    sasdie.Sasdie.add_marker = sasdie.Sasdie.ajoutMarqueurSurLaCarte
     sasdie.Sasdie.render_html = sasdie.Sasdie.produireHTMLCarte
 
-    sasdie.Sasdie.add_marker = sasdie.Sasdie.ajoutMarqueurSurLaCarte
 
-    # On utilise les variables d'environement pour stocker les identifiants
-    # EMAIL = os.environ.get('EMAIL')
-    # STUDENT_ID = os.environ.get('STUDENT_ID')
+# On utilise les variables d'environement pour stocker les identifiants
+EMAIL = os.environ.get('EMAIL')
+STUDENT_ID = os.environ.get('STUDENT_ID')
 
 
 def main():
-    c = sasdie.API(..., ...)
+    c = sasdie.API(EMAIL, STUDENT_ID)
 
     if not c.connect():
         print("Couldn't connect to the api.")
